dcfb992f707e3af9be11a2437b33f7aac971c84a
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-14 22:01:11 +0530

    fixed: token sequence overflow

--- server/model.py
+++ server/model.py
@@ -15,7 +15,7 @@ def saveTextAsImage(text: str):
 
     img = Image.new('RGB', (1000, 1000), (255, 255, 255))
     d1 = ImageDraw.Draw(img)
-    d1.text((1, 1), mainText.encode('cp1252'), fill=(255, 0, 0))
+    d1.text((1, 1), mainText.encode('utf-8'), fill=(255, 0, 0))
     img.show()
     img.save("files/result.png")
 
@@ -23,7 +23,10 @@ def saveTextAsImage(text: str):
 def getTextSummarization(TEXT: str):
     saveTextAsImage(TEXT)
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    data = summarizer(TEXT, max_length=130, min_length=50, do_sample=False)
+    try:
+        data = summarizer(TEXT, max_length=500, min_length=50, do_sample=False)
+    except IndexError:
+        return [{"summary_text": 'Sorry, the content is too large to be processed...'}]
     return data
 
 
