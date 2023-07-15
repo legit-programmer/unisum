9b9db17f8b6df9a84a9abccb71c6d1fc3cb42730
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-15 23:03:51 +0530

    fetch files ui design

--- app/src/components/Filesidebar.tsx
+++ app/src/components/Filesidebar.tsx
@@ -1,10 +1,13 @@
 import { AxiosInstance } from "axios";
+import { useState } from "react";
 
 interface props {
     client: AxiosInstance;
 }
 
 const Filesidebar = ({ client }: props) => {
+    const [fetching, setFetching] = useState(false);
+
     const uploadFile = (file: File) => {
         client
             .post(
@@ -26,6 +29,8 @@ const Filesidebar = ({ client }: props) => {
         }
     };
 
+    const fetchFile = () => {setFetching(true)};
+
     return (
         <div className=" bg-white drop-shadow-2xl w-[15%] h-[735px]">
             <input
@@ -39,10 +44,37 @@ const Filesidebar = ({ client }: props) => {
             <br />
             <label
                 htmlFor="file"
-                className=" transition-all w-[50%] items-center text-center font-modern text-sm text-red-300 m-10 p-2 rounded-md  ring-1 ring-red-300 hover:bg-red-300 hover:text-white hover:drop-shadow-lg"
+                className=" transition-all w-[50%] items-center text-center font-modern text-sm text-red-300 m-12 p-2 rounded-md  ring-1 ring-red-300 hover:bg-red-300 hover:text-white hover:drop-shadow-lg"
             >
                 Add Files
             </label>
+            <div className=" flex ml-12 mt-5">
+                <button
+                    className="w-[62%] transition:all bg-red-300 rounded-md p-1 text-white hover:bg-red-200"
+                    onClick={fetchFile}
+                >
+                    Refresh
+                </button>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className={
+                        fetching === true
+                            ? "w-6 h-6 text-gray-300 ml-2 mt-1 animate-spin"
+                            : "w-6 h-6 text-gray-300 ml-2 mt-1"
+                    }
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                </svg>
+            </div>
+
             <div className="w-full  absolute bottom-0 bg-[#54D0AB]">
                 <p className="font-modern text-white text-[13px] ml-6">
                     Made by
