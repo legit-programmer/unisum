02a2fe4bb3b39190da275ae32af80aaa8536fcf8
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-19 22:03:12 +0530

    added: link to github repo

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-[#54D0AB] h-14 drop-shadow-xl">
-        <div className="header text-white font-modern font-black text-4xl mx-8 p-2">
+    <div className="flex navbar bg-[#54D0AB] h-14 drop-shadow-xl relative items-center">
+        <div className="header text-white font-modern font-black text-4xl mx-8 p-2 drop-shadow-xl">
             <h1>Unisum.</h1>
         </div>
+        <img onClick={()=>window.open('https://github.com/legit-programmer/unisum/')} className="transition-all absolute right-5 w-10 cursor-pointer hover:w-12 hover:drop-shadow-lg" src="https://i.ibb.co/n7DG5fj/icons8-github-60.png" alt="" />
+        
     </div>
   )
 }
