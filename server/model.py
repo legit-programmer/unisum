1acd99c5e7b06b9637ce419e7a2688f24b25a637
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-21 17:57:53 +0530

    implementing langchain for text sum.

--- server/model.py
+++ server/model.py
@@ -2,13 +2,26 @@ from transformers import pipeline
 from PIL import Image, ImageDraw
 import pytesseract
 import os
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain import HuggingFaceHub
+from langchain.docstore.document import Document
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.environ.get('TOKEN')
+llm = HuggingFaceHub(repo_id='facebook/bart-large-cnn',
+                     model_kwargs={'temperature': 2}, huggingfacehub_api_token=TOKEN)
+
 
 class ImageFile:
     def __init__(self) -> None:
         self.file = None
 
 def ocr(file):
-    pytesseract.pytesseract.tesseract_cmd = os.path.join(os.getcwd(), 'tesseract/tesseract.exe')
+    pytesseract.pytesseract.tesseract_cmd = os.path.join(
+        os.getcwd(), 'tesseract/tesseract.exe')
 
     result = pytesseract.image_to_string(file)
     return result
@@ -30,30 +43,36 @@ def saveTextAsImage(text: str):
     d1.text((1, 1), mainText.encode('utf-8'), fill=(255, 0, 0))
     ImageFile.file = img
     ImageFile.file.show()
-    
+
 
 def getTextFromDotTxt(file):
     return str(file.read())[2:-1]
 
 
 def getTextSummarization(TEXT: str):
-    
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    try:
-        data = summarizer(TEXT, max_length=500, min_length=50, do_sample=False)
-    except IndexError:
-        return [{"summary_text": 'Sorry, the content is too large to be processed...'}]
-    return data
+
+    model = HuggingFaceHub(repo_id='facebook/bart-large-cnn',
+                           model_kwargs={'temperature': 0, 'min_length': 100, 'max_length': 500}, huggingfacehub_api_token=TOKEN)
+    text_splitter = CharacterTextSplitter()
+    chunks = text_splitter.split_text(TEXT)
+    doc = []
+    for text in chunks:
+        doc.append(Document(page_content=text))
+    print(doc)
+    # docs = [Document(page_content=text) for text in chunks]
+    summary_chain = load_summarize_chain(model, 'map_reduce')
+    result = summary_chain.run(doc)
+    return [{"summary_text": result}]
 
 
 def getAnswerFromImage(_question):
     model = pipeline("document-question-answering",
-                    model="naver-clova-ix/donut-base-finetuned-docvqa")
+                     model="naver-clova-ix/donut-base-finetuned-docvqa")
     return model(image=ImageFile.file, question=_question)
 
 
 def summarizeFromIllustration(file):
-    
+
     image_to_text = pipeline(
         "image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     return image_to_text(file)
--- server/textsum/views.py
+++ server/textsum/views.py
@@ -6,9 +6,9 @@ from model import getTextSummarization, saveTextAsImage
 @api_view(['POST'])
 def activate(request):
     data = dict(request.data)
-    print(data['text'])
+    # print(data['text'])
     saveTextAsImage(data['text'])
     result = getTextSummarization(data['text'])
-    print(result)
+    # print(result)
     return Response(result, 200)
 
