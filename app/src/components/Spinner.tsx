84fc4d4d6f307f7923d67df6275029cfd317b7af
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-09-04 19:30:49 +0530

    changed spinner animation

--- app/src/components/Spinner.tsx
+++ app/src/components/Spinner.tsx
@@ -3,7 +3,7 @@ const Spinner = () => {
         <div className="left-[11.5%] w-[63.5%] h-[92.75%] dark: absolute top-[7.%]  text-white flex items-center justify-center bg-white bg-opacity-100">
             <img
                 className=" absolute w-[25%] top-[30%]"
-                src="https://thumbs.gfycat.com/AchingSpeedyArmyworm-size_restricted.gif"
+                src="https://www.zilliondesigns.com/blog/wp-content/uploads/triangle-spinner.gif"
                 alt=""
             />
         </div>
